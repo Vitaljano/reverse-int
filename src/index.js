b7ad4731b6565eb4ca1adebdc81120f37dcb26e5
Vitaljano <=>
2023-08-04 00:43:51 +0300

    feat: done task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    const arr = n.toString().split("");
+    if (arr[0] === "-") {
+        return parseInt(arr.slice(1).reverse().join(""));
+    }
+    return arr.reverse().join("");
+};
